67452bb5be65db8d1969d6cf2ce18c1fa93bc130
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-30 21:58:46 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,8 +3,8 @@ require 'pry'
 def my_find(collection)
     i = 0 
     while i < collection.length 
-      return collection[i]
       if yield(collection[i])
+        return collection[i]
         
         
     end 
